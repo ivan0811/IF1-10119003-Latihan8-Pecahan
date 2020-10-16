790f947986cfef29eba249807a70aaf8ad5df61d
ivan0811 <ivanfaathirza@gmail.com>
2020-10-16 18:17:49 +0700

    initial commit

---
+++ src/com/pecahan/Pecahan.java
@@ -0,0 +1,20 @@
+package com.pecahan;
+/*
+	NAMA = Ivan Faathirza
+	KELAS = IF1
+	NIM = 10119003
+	Deskripsi program = program ini untung menampilkan bilangan pecahan float dan double
+ */
+public class Pecahan {
+
+    public static void main(String[] args) {
+	    float f;
+	    double d;
+	    
+		//max 7
+	    f = 1234567890.123456789F;
+	    d = 1_234_567_890.123456789;
+	    System.out.println("Float : "+f);
+	    System.out.println("Double : "+d);
+    }
+}
